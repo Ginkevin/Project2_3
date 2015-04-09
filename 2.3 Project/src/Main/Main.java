7f01d7fdd9cbcd4f344ab9c4e70155778200bc3b
Beun-de-Beunhaas <ke.venema@st.hanze.nl>
2015-04-09 11:18:57 +0200

    Splashscreen weer toegevoegd

--- 2.3 Project/src/Main/Main.java
+++ 2.3 Project/src/Main/Main.java
@@ -1,4 +1,6 @@
 package Main;
+import GUI.Splash;
+
 import java.io.IOException;
 
 import Network.Connect;
@@ -7,7 +9,16 @@ import Network.Connect;
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
+		
+		Splash splash = new Splash();    	
+    	try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			
+		}    
+		
 		Connect.getInstance().setConnection();
 		Worker arbeit = new Worker();
+		splash.dispose();
 	}
 }
